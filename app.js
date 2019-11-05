5a5a6577f5a93213579cc723a8626edbffbacde1
Anonymous <ebrahim994a@gmail.com>
2019-11-04 19:33:47 -0500

    using bcrypt with 10 salting rounds

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 //jshint esversion:6
 //dependencies
+
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
-
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 //Connect the mongoose database using the new url parser flag
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser:true})
+
 
 // A schema to be followed by documents in a collection
 const userSchema = mongoose.Schema({
@@ -15,9 +20,8 @@ const userSchema = mongoose.Schema({
   password:String
 });
 
-// encrypting the database with AES
-const secret = 'thisisoursecret.'
-userSchema.plugin(encrypt, {secret:secret, encryptedFields:['password']});
+// encrypting the database with AES (using bcrypt instead of AES encryption)
+// userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:['password']});
 
 //A collection in the userDB database
 const User = mongoose.model('user', userSchema)
@@ -48,19 +52,22 @@ app.get('/register', function(req, res){
 //post using the register route (creating a new user)
 app.post('/register', function(req, res){
 
+//Hashing our password
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    })
   //adding a document in the user collection
-  const newUser = new User({
-    email:req.body.username,
-    password:req.body.password
-  })
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render('secrets')
+      }
+    })
+  });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render('secrets')
-    }
-  })
 });
 
 app.post('/login', function(req,res){
@@ -68,17 +75,20 @@ app.post('/login', function(req,res){
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email:username, password:password}, function(err, found){
+  User.findOne({email:username}, function(err, foundUser){
     if(!err){
-      if(found){
-        if(found.password === password){
-          res.render('secrets')
+      if(foundUser){
+        bcrypt.compare(password,foundUser.password,function(err,result){
+        if(result){
+          res.render('secrets');
         }
-        }
-    }else
-    {console.log(err)}
+      })}}else{
+        console.log(err);
+      }
+    })
   })
-})
+
+
 
 
 app.listen(3000, function(req,res){
