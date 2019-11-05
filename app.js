22851cf248e6f09256af3e302091efcdf3715b9a
Anonymous <ebrahim994a@gmail.com>
2019-11-05 14:37:22 -0500

    addedauthentication using passport

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 //dependencies
-
+//session branch
 require('dotenv').config();
 
 const express = require('express');
@@ -8,11 +8,29 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
-const bcrypt = require('bcrypt')
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+//initialize an express server and call it app
+let app = express()
+
+//Setting up everything we need
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+  secret:'our little secret',
+  resave:false,
+  saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// encrypting the database with AES
+// userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:['password']});
 //Connect the mongoose database using the new url parser flag
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser:true})
-
 
 // A schema to be followed by documents in a collection
 const userSchema = mongoose.Schema({
@@ -20,19 +38,18 @@ const userSchema = mongoose.Schema({
   password:String
 });
 
-// encrypting the database with AES (using bcrypt instead of AES encryption)
-// userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:['password']});
+
+userSchema.plugin(passportLocalMongoose);
 
 //A collection in the userDB database
 const User = mongoose.model('user', userSchema)
 
-//initialize an express server and call it app
-let app = express()
+// using passport-local-mongoose
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-//Setting up ejs and allowing the express server to use bodyParser
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+mongoose.set('useCreateIndex', true);
 
 //home route
 app.get('/', function(req, res){
@@ -49,43 +66,48 @@ app.get('/register', function(req, res){
   res.render('register');
 })
 
+//secrets route
+app.get('/secrets',function(req,res){
+  if(req.isAuthenticated()){
+    res.render('secrets');
+  }else{
+    res.redirect('/login')
+  }
+});
+
 //post using the register route (creating a new user)
 app.post('/register', function(req, res){
 
-//Hashing our password
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-    const newUser = new User({
-      email:req.body.username,
-      password:hash
-    })
-  //adding a document in the user collection
-    newUser.save(function(err){
-      if(err){
-        console.log(err);
-      }else{
-        res.render('secrets')
-      }
-    })
-  });
-
+  User.register({username: req.body.username}, req.body.password, function(err,user){
+    if(err){
+      console.log(err);
+      res.redirect('/register')
+    }else{
+      passport.authenticate('local')(req,res,function(){
+        res.redirect('/secrets')
+      })
+    }
+    }
+  )
 });
 
+// login route
 app.post('/login', function(req,res){
 
-  const username = req.body.username;
-  const password = req.body.password;
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
 
-  User.findOne({email:username}, function(err, foundUser){
-    if(!err){
-      if(foundUser){
-        bcrypt.compare(password,foundUser.password,function(err,result){
-        if(result){
-          res.render('secrets');
-        }
-      })}}else{
-        console.log(err);
-      }
-    })
+  req.login(user,function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate('local')(req,res,function(){
+        res.redirect('/secrets');
+      })
+    }
+  })
   })
 
 
